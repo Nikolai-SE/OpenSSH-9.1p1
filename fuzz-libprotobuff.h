0424db2dbce0930b9e6beda663b5be1f3b21f630
nik <nikolai.stoiko@yandex.ru>
2023-06-14 13:32:46 +0300

    Add fuzz-libprotobuff.cc fuzz-libprotobuff.h update to run in thread

--- fuzz-libprotobuff.h
+++ fuzz-libprotobuff.h
@@ -4,3 +4,17 @@
 
 // comment to build sshd and comment LIBFUZZER_FLAG in Makefile
 #define FUZZ_LIBPROTOBUFF
+
+
+/* FOR FUZZING */
+
+#define FUZZING_PTHREAD_EXIT
+
+#ifdef FUZZING_PTHREAD_EXIT
+#define _OPEN_THREADS
+#include <pthread.h>  // sudo apt-get install libpthread-stubs0-dev
+//#define pthread_exit(x) pthread_exit(x)
+//#define pthread_exit(x); pthread_exit(x);
+//#define _exit(x) pthread_exit(x)
+//#define _exit(...); pthread_exit(__VA_ARGS__);
+#endif
